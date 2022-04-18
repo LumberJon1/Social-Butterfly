5bfc6fe0633e2f03932aee4b84a428daac961a9a
Jonathan W <jwilliams12117@gmail.com>
2022-04-17 18:59:37 -0700

    Finished route functionality for user API routes delete and put

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -5,38 +5,84 @@ const User = require("../models/User");
 router.get("/", (req, res) => {
     User.find({})
     // TODO: Populate thoughts for each user
-        .select("-__v")
-        .sort({_id: -1})
-        .then(userData => res.json(userData))
-        .catch((err) => {
-            console.log(err)
-            res.status(400).json(err)
-        });
+    .select("-__v")
+    .sort({_id: -1})
+    .then(userData => res.json(userData))
+    .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 
 // GET a single user
 router.get("/:id", (req, res) => {
-
+    User.findOne({
+        _id: req.params.id
+    })
+    // TODO: Populate thoughts for each user
+    .select("-__v")
+    .then((userData) => {
+        if (!userData) {
+            res.status(404).json("No user with this ID");
+            return;
+        }
+        res.json(userData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 
 // POST Create a new user
 router.post("/", (req, res) => {
     User.create(req.body)
-        .then(userData => res.json(userData))
-        .catch((err) => {
-            console.log(err)
-            res.status(400).json(err)
-        });
+    .then(userData => res.json(userData))
+    .catch((err) => {
+        console.log(err)
+        res.status(400).json(err)
+    });
 });
 
 // PUT update a user
 router.put("/:id", (req, res) => {
-
+    User.findOneAndUpdate(
+        {
+            _id: req.params.id
+        },
+        req.body,
+        {
+            new: true
+        }
+    )
+    .select("-__v")
+    .then(userData => {
+        if (!userData) {
+            res.status(400).json("No user with this ID");
+            return;
+        }
+        res.json(userData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 
 // DELETE a user
 router.delete("/:id", (req, res) => {
-
+    User.findOneAndDelete({_id: req.params.id})
+    .then(userData => {
+        if (!userData) {
+            res.status(400).json("No user with this ID");
+            return;
+        }
+        res.json(userData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 
 module.exports = router;
